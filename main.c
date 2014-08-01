bd6eaa7b75f20c7a9f503bf0d5a8d33050d6b22c
swpd <swpdtz@gmail.com>
2014-08-01 11:29:14 +0000

    remove coro package and use dthread api of duda instead
    Signed-off-by: swpd <swpdtz@gmail.com>

--- main.c
+++ main.c
@@ -1,21 +1,20 @@
 #include "webservice.h"
-#include "packages/coro/coro.h"
 
-DUDA_REGISTER("Coro Fibonacci Examples", "coro fibonacci demo");
+DUDA_REGISTER("Dthread Fibonacci Examples", "dthread fibonacci demo");
 
 struct bundle {
-    channel_t *chan;
+    duda_dthread_channel_t *chan;
     duda_request_t *dr;
 };
 
 void consumer(void *data)
 {
     struct bundle *bdl = data;
-    channel_t *chan = bdl->chan;
+    duda_dthread_channel_t *chan = bdl->chan;
     duda_request_t *dr = bdl->dr;
     response->http_status(dr, 200);
-    while (!coro->chan_done(chan)) {
-        int *n = coro->chan_recv(chan);
+    while (!dthread->chan_done(chan)) {
+        int *n = dthread->chan_recv(chan);
         response->printf(dr, "%d\n", *n);
         monkey->mem_free(n);
     }
@@ -24,7 +23,7 @@ void consumer(void *data)
 
 void productor(void *data)
 {
-    channel_t *chan = data;
+    duda_dthread_channel_t *chan = data;
     int num1 = 1;
     int num2 = 1;
     int i;
@@ -39,30 +38,29 @@ void productor(void *data)
             num1 = num2;
             num2 = *n;
         }
-        coro->chan_send(chan, n);
+        dthread->chan_send(chan, n);
     }
-    coro->chan_end(chan);
+    dthread->chan_end(chan);
 }
 
 void cb_fibonacci(duda_request_t *dr)
 {
     // unbuffered channel
-    channel_t *chan = coro->chan_create(0, monkey->mem_free);
+    duda_dthread_channel_t *chan = dthread->chan_create(0, monkey->mem_free);
     struct bundle *bdl = monkey->mem_alloc(sizeof(*bdl));
     bdl->chan = chan;
     bdl->dr = dr;
-    int cid = coro->create(consumer, bdl);
-    int pid = coro->create(productor, chan);
-    coro->chan_set_sender(chan, pid);
-    coro->chan_set_receiver(chan, cid);
-    coro->resume(cid);
-    coro->chan_free(chan);
+    int cid = dthread->create(consumer, bdl);
+    int pid = dthread->create(productor, chan);
+    dthread->chan_set_sender(chan, pid);
+    dthread->chan_set_receiver(chan, cid);
+    dthread->resume(cid);
+    dthread->chan_free(chan);
     monkey->mem_free(bdl);
 }
 
 int duda_main()
 {
-    duda_load_package(coro, "coro");
     map->static_add("/", "cb_fibonacci");
     return 0;
 }
